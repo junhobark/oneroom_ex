0988a8a3109cd093b1eee6a7d4ee708930ea65e9
junhobark <wnsh66@naver.com>
2023-06-06 18:25:44 +0900

    ..

--- lib/Info/info_written_board.dart
+++ lib/Info/info_written_board.dart
@@ -1,23 +1,193 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:oneroom_ex/common/default_layout.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:oneroom_ex/common/colors.dart';
+import 'package:oneroom_ex/community/GENERAL/general_board_postId.dart';
+import 'package:provider/provider.dart';
+import '../login/uid_provider.dart';
 
-class WrittenBoardScreen extends StatelessWidget {
+class WrittenBoardScreen extends StatefulWidget {
   const WrittenBoardScreen({Key? key}) : super(key: key);
 
   @override
+  State<WrittenBoardScreen> createState() => _WrittenBoardScreenState();
+}
+
+class _WrittenBoardScreenState extends State<WrittenBoardScreen> {
+  List<dynamic> posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    //final url = Uri.parse('http://10.0.2.2:8080/user/abc/post');
+    final url = Uri.parse('http://10.0.2.2:8080/user/${Provider.of<UIDProvider>(context, listen: false).uid}/post');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = utf8.decode(response.bodyBytes); // UTF-8로 디코딩
+
+      setState(() {
+        posts = json.decode(data);
+      });
+    } else {
+      print('Failed to fetch posts. Error: ${response.statusCode}');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultLayout(
-      title: '내가 쓴 글',
-      child: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text(
+          '내가 쓴 글',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+
+      body: Container(
+        color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(child: Text('내가 쓴 글')),
-              ],
-            ),
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              return Container(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 8),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            GeneralBoardPostId(post['id']), //id값을 넘김
+                      ),
+                    ).then((value) {
+                      setState(() {
+                        fetchPosts();
+                      });
+                    });
+                  },
+                  title: Text(
+                    '${post['title'].length > 20 ? '${post['title'].substring(0, 18)} ⋯' : post['title']}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 1),
+                      Text(
+                        '${post['body'].length > 20 ? '${post['body'].substring(0, 20)} ⋯' : post['body']}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16.0,
+                          color: BODY_TEXT_COLOR,
+                        ),
+                      ),
+                      SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.thumb_up_alt_outlined,
+                                size: 16,
+                                color: Colors.red,
+                              ),
+                              SizedBox(width: 3),
+                              Text(
+                                '${post['likes']}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.mode_comment_outlined,
+                                size: 15,
+                                color: Colors.blue,
+                              ),
+                              SizedBox(width: 3),
+                              Text(
+                                '${post['commentCount']}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '|',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                          Text(
+                            DateFormat('HH:mm').format(DateTime.parse(post['createdAt'])),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '|',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '${post['authorName'].replaceFirst('경남 진주시 ', '')}***',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
--- lib/Info/info_written_comment.dart
+++ lib/Info/info_written_comment.dart
@@ -1,24 +1,193 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:oneroom_ex/common/colors.dart';
+import 'package:oneroom_ex/community/GENERAL/general_board_postId.dart';
+import 'package:provider/provider.dart';
+import '../login/uid_provider.dart';
 
-import '../common/default_layout.dart';
-
-class WrittenCommentScreen extends StatelessWidget {
+class WrittenCommentScreen extends StatefulWidget {
   const WrittenCommentScreen({Key? key}) : super(key: key);
 
   @override
+  State<WrittenCommentScreen> createState() => _WrittenCommentScreenState();
+}
+
+class _WrittenCommentScreenState extends State<WrittenCommentScreen> {
+  List<dynamic> comments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchComments();
+  }
+
+  Future<void> fetchComments() async {
+    //final url = Uri.parse('http://10.0.2.2:8080/user/abc/comment');
+    final url = Uri.parse('http://10.0.2.2:8080/user/${Provider.of<UIDProvider>(context, listen: false).uid}/comment');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = utf8.decode(response.bodyBytes); // UTF-8로 디코딩
+
+      setState(() {
+        comments = json.decode(data);
+      });
+    } else {
+      print('Failed to fetch posts. Error: ${response.statusCode}');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultLayout(
-      title: '내가 쓴 댓글',
-      child: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text(
+          '내가 쓴 댓글',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+
+      body: Container(
+        color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(child: Text('내가 쓴 댓글')),
-              ],
-            ),
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: comments.length,
+            itemBuilder: (context, index) {
+              final comment = comments[index];
+              return Container(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 8),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            GeneralBoardPostId(comment['id']), //id값을 넘김
+                      ),
+                    ).then((value) {
+                      setState(() {
+                        fetchComments();
+                      });
+                    });
+                  },
+                  title: Text(
+                    '${comment['title'].length > 20 ? '${comment['title'].substring(0, 18)} ⋯' : comment['title']}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 1),
+                      Text(
+                        '${comment['body'].length > 20 ? '${comment['body'].substring(0, 20)} ⋯' : comment['body']}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16.0,
+                          color: BODY_TEXT_COLOR,
+                        ),
+                      ),
+                      SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.thumb_up_alt_outlined,
+                                size: 16,
+                                color: Colors.red,
+                              ),
+                              SizedBox(width: 3),
+                              Text(
+                                '${comment['likes']}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.mode_comment_outlined,
+                                size: 15,
+                                color: Colors.blue,
+                              ),
+                              SizedBox(width: 3),
+                              Text(
+                                '${comment['commentCount']}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '|',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                          Text(
+                            DateFormat('HH:mm').format(DateTime.parse(comment['createdAt'])),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '|',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            '${comment['authorName'].replaceFirst('경남 진주시 ', '')}***',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
